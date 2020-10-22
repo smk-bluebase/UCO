c3d960ba2db875f5e300e8f583120b80583bfb72
Mohana Krishnan S <mohanakrishnan.s@bluebase.in>
2020-10-22 15:21:24 +0530

    CommonUtils change

--- UCO/app/src/main/java/com/example/mypc/uco/CommonUtils.java
+++ UCO/app/src/main/java/com/example/mypc/uco/CommonUtils.java
@@ -1,7 +1,8 @@
 package com.example.mypc.uco;
 
 public class CommonUtils {
-    static String IP = "https://www.ucosocietychennai.in";
+//    static String IP = "https://www.ucosocietychennai.in";
+static String IP = "http://192.168.201.165";
 
     public String StringToRound(String str_value){
         double new_value;
